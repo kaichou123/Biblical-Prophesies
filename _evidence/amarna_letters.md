688c248b7d3da5ee537831e5ae3c8dea0377b7cc
jandjmathis <jandjmathis@gmail.com>
2025-05-08 21:12:04 -0700

    grok recommendations

--- _evidence/amarna_letters.md
+++ _evidence/amarna_letters.md
@@ -1,6 +1,5 @@
 ---
 title: Amarna Letters - Canaanite Correspondence
-image: assets/images/amarna_tablet.jpg
 layout: evidence
 evidence_id: E0001
 type: Archaeological
@@ -9,3 +8,9 @@ description: Letters mentioning Bethlehem as a Canaanite settlement.
 related_prophecies: [00001]
 ---
 The Amarna Letters confirm Bethlehem’s existence, supporting Micah 5:2.
+
+<div class="thumbnail">
+  <a href="{{ site.baseurl }}/assets/images/amarna_tablet.jpg" class="lightbox-trigger">
+    <img src="{{ site.baseurl }}/assets/images/amarna_tablet.jpg" alt="Amarna Tablet" class="thumbnail-img">
+  </a>
+</div>
--- assets/js/table.js
+++ assets/js/table.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    // Existing DataTables initialization
     if ($('#prophecy-table').length) {
         $('#prophecy-table').DataTable({
             paging: true,
@@ -13,4 +14,24 @@ $(document).ready(function() {
             ordering: true
         });
     }
+
+    // Lightbox functionality
+    $('.lightbox-trigger').on('click', function(e) {
+        e.preventDefault(); // Prevent the default link behavior
+        var imgSrc = $(this).attr('href'); // Get the full-size image URL
+        $('#lightbox-img').attr('src', imgSrc); // Set the modal image source
+        $('#lightbox').fadeIn(); // Show the modal
+    });
+
+    // Close the lightbox when the close button is clicked
+    $('.close').on('click', function() {
+        $('#lightbox').fadeOut(); // Hide the modal
+    });
+
+    // Close the lightbox when clicking outside the image
+    $('#lightbox').on('click', function(e) {
+        if (e.target === this) { // Only close if clicking the background
+            $('#lightbox').fadeOut();
+        }
+    });
 });
